8589a6f60de6884e9bf52088c6f8bd15a7a8b4b2
oriol <farre.lizana.oriol@gmail.com>
2017-02-27 17:04:40 +0100

    Footer licence

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,21 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <div class="navbar-text navbar-right">
+          <div class="col-md-6">
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+              <img alt="Llicència de Creative Commons" style="border-width:0; font-size:0.6em; padding:0px;" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
+            </a>
+            <br />
+            <span xmlns:dct="http://purl.org/dc/terms/" style="font-size:0.6em;" property="dct:title">
+              Learneverywhere
+            </span>
+            <span style="font-size:0.6em;">
+             està subjecte a una llicència de
+            </span>
+             <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" style="font-size:0.6em;">
+               Reconeixement-NoComercial-CompartirIgual 4.0 Internacional de Creative Commons</a>
+          </div>
+          <div class="col-md-6 navbar-text navbar-right">
             Learneverywhere 2017
           </div>
         </div>
