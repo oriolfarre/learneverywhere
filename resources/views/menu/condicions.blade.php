b213c1e845db737b6ba7efdda43897ced899c7bb
Unknown <raulitus78@hotmail.com>
2017-03-11 12:32:55 +0100

    Modal & canvis condicions

--- resources/views/menu/condicions.blade.php
+++ resources/views/menu/condicions.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -12,28 +13,27 @@
               <div class="normes">
               En utilitzar els serveis, vostè no pot publicar o enviar res que:
                 <div class="conducta">
-                  <li> Sigui desagradable, intimidi a algú o tingui l'objectiu d'assetjar, espantar o ofendre a algú.</li>
-                  <li> Estigui intencionadament pensat per provocar o enemistar a la gent, especialment les provocacions.</li>
-                  <li> Impliqui paraulotes o tingui l'objectiu d'ofendre o avergonyir a algú.</li>
-                  <li> Fomenti l'autolesió o activitats perilloses o il·legals.</li>
-                  <li> Representi alguna cosa terrible, escandalós o anguniant.</li>
-                  <li> Sigui obscè o pornogràfic, contingui imatges de persones nues, sigui de contingut sexual o representi algun tipus de violència gràfica.</li>
-                  <li> Sigui racista o discrimini a algú per la seva raça, religió, ètnia, edat, gènere, discapacitat o sexualitat.</li>
-                  <li> Infringeixi els drets de privacitat d'una altra persona.</li>
-                  <li> Sigui il·legal, pugui derivar en una responsabilitat legal per Learneverywhere o animi a la gent a implicar-s'hi en qualsevol cosa que sigui il·legal (per exemple drogues, violència o crims).</li>
-                  <li> Sigui difamatori o violi els drets de tercers, com un abús de confiança, o la vulneració de drets d'autor, marca registrada, patent, secret comercial, dret moral, dret de privacitat, dret de publicitat o qualsevol altre dret de propietat intel·lectual.</li>
-                  <li> Constitueixi spam o un intent de vendre alguna cosa a altres usuaris, o competeixi amb el negoci de Learneverywhere.</li>
-                  <li> Contingui algun virus informàtic o altres codis maliciosos dissenyats per atacar, danyar, desviar, controlar, deshabilitar, sobrecarregar o perjudicar els serveis de qualsevol altre mode.</li>
-                  <li> Violi qualsevol dels protocols d'exclusió de robots del lloc, si existeix, o eviti altres mesures utilitzades per evitar o limitar l'accés als serveis.</li>
-                  <li> Ens reservem el dret, en qualsevol moment i sense previ avís, a eliminar o inhabilitar l'accés a qualsevol contingut, per qualsevol motiu o sense motiu.</li>
-                  <li> Vostè reconeix que no estem obligats a supervisar el seu accés o ús dels serveis, ni a revisar o modificar tots els continguts. Tot i això, tenim dret de fer-ho amb el fi d'operar i millorar els serveis.</li>
+                  <li class="normes"> Sigui desagradable, intimidi a algú o tingui l'objectiu d'assetjar, espantar o ofendre a algú.</li>
+                  <li class="normes"> Estigui intencionadament pensat per provocar o enemistar a la gent, especialment les provocacions.</li>
+                  <li class="normes"> Impliqui paraulotes o tingui l'objectiu d'ofendre o avergonyir a algú.</li>
+                  <li class="normes"> Fomenti l'autolesió o activitats perilloses o il·legals.</li>
+                  <li class="normes"> Representi alguna cosa terrible, escandalós o anguniant.</li>
+                  <li class="normes"> Sigui obscè o pornogràfic, contingui imatges de persones nues, sigui de contingut sexual o representi algun tipus de violència gràfica.</li>
+                  <li class="normes"> Sigui racista o discrimini a algú per la seva raça, religió, ètnia, edat, gènere, discapacitat o sexualitat.</li>
+                  <li class="normes"> Infringeixi els drets de privacitat d'una altra persona.</li>
+                  <li class="normes"> Sigui il·legal, pugui derivar en una responsabilitat legal per Learneverywhere o animi a la gent a implicar-s'hi en qualsevol cosa que sigui il·legal (per exemple drogues, violència o crims).</li>
+                  <li class="normes"> Sigui difamatori o violi els drets de tercers, com un abús de confiança, o la vulneració de drets d'autor, marca registrada, patent, secret comercial, dret moral, dret de privacitat, dret de publicitat o qualsevol altre dret de propietat intel·lectual.</li>
+                  <li class="normes"> Constitueixi spam o un intent de vendre alguna cosa a altres usuaris, o competeixi amb el negoci de Learneverywhere.</li>
+                  <li class="normes"> Contingui algun virus informàtic o altres codis maliciosos dissenyats per atacar, danyar, desviar, controlar, deshabilitar, sobrecarregar o perjudicar els serveis de qualsevol altre mode.</li>
+                  <li class="normes"> Violi qualsevol dels protocols d'exclusió de robots del lloc, si existeix, o eviti altres mesures utilitzades per evitar o limitar l'accés als serveis.</li>
+                  <li class="normes"> Ens reservem el dret, en qualsevol moment i sense previ avís, a eliminar o inhabilitar l'accés a qualsevol contingut, per qualsevol motiu o sense motiu.</li>
+                  <li class="normes"> Vostè reconeix que no estem obligats a supervisar el seu accés o ús dels serveis, ni a revisar o modificar tots els continguts. Tot i això, tenim dret de fer-ho amb el fi d'operar i millorar els serveis.</li>
 
                 </div>
-
-
-            </div>
-        </div>
-    </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
 
 @endsection
