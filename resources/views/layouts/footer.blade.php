ffcdd56b735a6358bb324d8642864f7e88fb69ad
Unknown <alexander.bruno1995@gmail.com>
2017-03-11 13:47:00 +0100

    Footer Nosaltres
    footer y nosaltres

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <div class="col-md-6">
+          <div class="col-md-6 navbar-text navbar-left">
             <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
               <img alt="Llicència de Creative Commons" style="border-width:0; font-size:0.6em; padding:0px;" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
             </a>
@@ -17,7 +17,11 @@
              <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" style="font-size:0.6em;">
                Reconeixement-NoComercial-CompartirIgual 4.0 Internacional de Creative Commons</a>
           </div>
-          <div class="col-md-6 navbar-text navbar-right">
+          <div class="col-3 navbar-text navbar-center">
+            <img src="puncat.png"  height="100" width="100">
+            
+          </div>
+          <div class="col-3 navbar-text navbar-right">
             Learneverywhere 2017
           </div>
         </div>
