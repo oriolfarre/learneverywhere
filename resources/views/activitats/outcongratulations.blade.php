b262aea895f572dbfaefd5b5dfd415a23de23ff9
oriol <farre.lizana.oriol@gmail.com>
2017-03-11 17:43:58 +0100

    Correc ortografica

--- resources/views/activitats/outcongratulations.blade.php
+++ resources/views/activitats/outcongratulations.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h1>ENHORABONA!</h1>
-            <h2>Has superat el nivell, si vols gaudir de totes les funcións, registrat!</h2>
+            <h2>Has superat el nivell, si vols gaudir de totes les funcions, registrat!</h2>
             <a class="btn btn-primary btn-lg" href="{{ url('/register') }}" role="button">Registrar</a>
         </div>
     </div>
